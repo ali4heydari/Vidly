da60f078c9058b5f207624c70ed08a34591cd942
Ali Heydari <ali4heydari@gmail.com>
2019-02-14 00:56:24 +0330

    CamelCaseing API

--- Vidly/App_Start/WebApiConfig.cs
+++ Vidly/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Vidly
 {
@@ -11,9 +13,15 @@ namespace Vidly
         {
             // Web API configuration and services
 
+            JsonSerializerSettings settings = config.Formatters.JsonFormatter.SerializerSettings;
+            
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+            
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
