f715c3bab0e5daa85a6d644e882a35e5bc22aec8
Ali Heydari <ali4heydari@gmail.com>
2019-02-15 13:10:36 +0330

    Add OutPutCache to home

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace Vidly.Controllers
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        [OutputCache(Duration = 0,VaryByParam = "*",NoStore = true)]
         public ActionResult Index()
         {
             return View();
