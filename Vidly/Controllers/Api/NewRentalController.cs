c5fc9da8f37606ccfd1be25f4d88ce05e140c49f
Ali Heydari <ali4heydari@gmail.com>
2019-02-16 23:12:26 +0330

    Checking edge cases

--- Vidly/Controllers/Api/NewRentalController.cs
+++ Vidly/Controllers/Api/NewRentalController.cs
@@ -19,6 +19,11 @@ namespace Vidly.Controllers.Api
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRentalDto)
         {
+            if (newRentalDto.MoviesId.Any())
+            {
+                return BadRequest("No movie Ids have been given");
+            }
+
             Customer customer = _context.Customers.SingleOrDefault(c => c.Id == newRentalDto.CustomerId);
 
             if (customer == null)
@@ -26,16 +31,27 @@ namespace Vidly.Controllers.Api
                 return BadRequest("Invalid customer ID");
             }
 
-            IQueryable<Movie> movies = _context
+
+            List<Movie> movies = _context
                 .Movies
                 .Where(m => newRentalDto
                     .MoviesId
-                    .Contains(m.Id));
+                    .Contains(m.Id)).ToList();
+
+            if (movies.Count != newRentalDto.MoviesId.Count)
+            {
+                return BadRequest("One or more movieIds are invalid");
+            }
 
             foreach (Movie movie in movies)
             {
+                if (movie.NumberAvailable == 0)
+                {
+                    return BadRequest("Movie is not available");
+                }
+
                 movie.NumberAvailable--;
-                
+
                 Rental rental = new Rental()
                 {
                     Customer = customer,
